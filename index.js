eb5a31df939b599f17804e788e9007f0faf23ff3
Steven Bloch <snbloch@gmail.com>
2020-10-27 02:33:26 -0700

    Forgot the messageReaction.message object in the remove listener

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ discordClient.on('messageReactionAdd', (messageReaction, user) => {
 });
 
 discordClient.on('messageReactionRemove', (messageReaction, user) => {
-    if (messageReaction && user && messageReaction.message && messageReaction.emoji && messageReaction.emoji.id && messageReaction.message.author && !messageReaction.message.author.bot && user.id && !user.bot && messageReaction.guild.emojis.resolve(messageReaction.emoji.id)) {
+    if (messageReaction && user && messageReaction.message && messageReaction.emoji && messageReaction.emoji.id && messageReaction.message.author && !messageReaction.message.author.bot && user.id && !user.bot && messageReaction.message.guild.emojis.resolve(messageReaction.emoji.id)) {
         let params = {
             ExpressionAttributeNames: {
                 '#ic': 'itemCount'
