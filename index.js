cfc08be765e4a736b34bbb402efb1561a4cc02ba
Steven Bloch <snbloch@gmail.com>
2020-10-27 02:14:48 -0700

    Don't need the debugging stuff after all, the code would have worked before but the messageReactionAdd event only fires for messages in the cache (message sent after bot started running)!

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ discordClient.once('ready', () => {
 });
 
 discordClient.on('messageReactionAdd', (messageReaction, user) => {
-    if (messageReaction && user && messageReaction.message && messageReaction.emoji && messageReaction.emoji.id && messageReaction.message.author && !messageReaction.message.author.bot && user.id && !user.bot) {
-        console.log(messageReaction);
-        console.log(messageReaction.message);
-        console.log(messageReaction.message.guild);
-        console.log(messageReaction.message.guild.emojis);
-        console.log(messageReaction.message.guild.emojis.resolve(messageReaction.emoji.id));
-        console.log(messageReaction.message.guild.emojis.resolve(messageReaction.emoji.id).identifier);
+    if (messageReaction && user && messageReaction.message && messageReaction.emoji && messageReaction.emoji.id && messageReaction.message.author && !messageReaction.message.author.bot && user.id && !user.bot && messageReaction.message.guild.emojis.resolve(messageReaction.emoji.id)) {
         let params = {
             ExpressionAttributeNames: {
                 '#ic': 'itemCount',
