4077dfa987a1c8860e9f773ccc5002b0fec51290
Steven Bloch <snbloch@gmail.com>
2020-10-27 22:48:19 -0700

    Rekeying with prefix, adding a 4th dimension, and adding a usage auto-response

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ discordClient.on('messageReactionAdd', (messageReaction, user) => {
                 ':zero': 0
             }, 
             Key: {
-                reactionKey: messageReaction.message.guild.id + '#' + messageReaction.message.channel.id,
+                reactionKey: 'userByChannel#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id,
                 subKey: user.id
             },
             TableName: config.awsDynamoDBTableName,
@@ -52,7 +52,7 @@ discordClient.on('messageReactionAdd', (messageReaction, user) => {
                 ':zero': 0
             }, 
             Key: {
-                reactionKey: messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + user.id,
+                reactionKey: 'emojiByChannel#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id,
                 subKey: messageReaction.emoji.id
             },
             TableName: config.awsDynamoDBTableName,
@@ -74,7 +74,29 @@ discordClient.on('messageReactionAdd', (messageReaction, user) => {
                 ':zero': 0
             }, 
             Key: {
-                reactionKey: messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + messageReaction.emoji.id,
+                reactionKey: 'emojiByUser#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + user.id,
+                subKey: messageReaction.emoji.id
+            },
+            TableName: config.awsDynamoDBTableName,
+            UpdateExpression: 'SET #ic = if_not_exists(#ic, :zero) + :incr, #lu = :lu'
+        };
+        dynamo.update(params, function(err, data){
+            if (err) {
+                console.error(err);
+            }
+        });
+        params = {
+            ExpressionAttributeNames: {
+                '#ic': 'itemCount',
+                '#lu': 'lastUsed'
+            },
+            ExpressionAttributeValues: {
+                ':incr': 1,
+                ':lu': new Date().toString(),
+                ':zero': 0
+            }, 
+            Key: {
+                reactionKey: 'userByEmoji#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + messageReaction.emoji.id,
                 subKey: user.id
             },
             TableName: config.awsDynamoDBTableName,
@@ -98,7 +120,7 @@ discordClient.on('messageReactionRemove', (messageReaction, user) => {
                 ':decr': 1
             }, 
             Key: {
-                reactionKey: messageReaction.message.guild.id + '#' + messageReaction.message.channel.id,
+                reactionKey: 'userByChannel#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id,
                 subKey: user.id
             },
             TableName: config.awsDynamoDBTableName,
@@ -118,7 +140,7 @@ discordClient.on('messageReactionRemove', (messageReaction, user) => {
                 ':decr': 1
             }, 
             Key: {
-                reactionKey: messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + user.id,
+                reactionKey: 'emojiByChannel#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id,
                 subKey: messageReaction.emoji.id
             },
             TableName: config.awsDynamoDBTableName,
@@ -138,7 +160,27 @@ discordClient.on('messageReactionRemove', (messageReaction, user) => {
                 ':decr': 1
             }, 
             Key: {
-                reactionKey: messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + messageReaction.emoji.id,
+                reactionKey: 'emojiByUser#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + user.id,
+                subKey: messageReaction.emoji.id
+            },
+            TableName: config.awsDynamoDBTableName,
+            UpdateExpression: 'SET #ic = #ic - :decr',
+            ConditionExpression: '#ic >= :decr'
+        };
+        dynamo.update(params, function(err, data){
+            if (err) {
+                console.error(err);
+            }
+        });
+        params = {
+            ExpressionAttributeNames: {
+                '#ic': 'itemCount'
+            },
+            ExpressionAttributeValues: {
+                ':decr': 1
+            }, 
+            Key: {
+                reactionKey: 'userByEmoji#' + messageReaction.message.guild.id + '#' + messageReaction.message.channel.id + '#' + messageReaction.emoji.id,
                 subKey: user.id
             },
             TableName: config.awsDynamoDBTableName,
@@ -154,10 +196,10 @@ discordClient.on('messageReactionRemove', (messageReaction, user) => {
 });
 
 discordClient.on('message', message => {
-    if (message && message.guild && message.guild.id && message.content && message.channel && message.channel.id && message.content.toLowerCase().trim() === '!reacts') {
+    if (message && message.guild && message.guild.id && message.content && message.channel && message.channel.id && message.content.toLowerCase().trim() === '!reacts users') {
         let params = {
             ExpressionAttributeValues: {
-                ':rk': message.guild.id + '#' + message.channel.id
+                ':rk': 'userByChannel#' + message.guild.id + '#' + message.channel.id
             }, 
             KeyConditionExpression: 'reactionKey = :rk',
             TableName: config.awsDynamoDBTableName
@@ -194,11 +236,51 @@ discordClient.on('message', message => {
         });
         message.delete();
     }
+    else if (message && message.guild && message.guild.id && message.content && message.channel && message.channel.id && message.content.toLowerCase().trim() === '!reacts emojis') {
+        let params = {
+            ExpressionAttributeValues: {
+                ':rk': 'emojiByChannel#' + message.guild.id + '#' + message.channel.id
+            }, 
+            KeyConditionExpression: 'reactionKey = :rk',
+            TableName: config.awsDynamoDBTableName
+        };
+        dynamo.query(params, function(err, data) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                data.Items.sort((a, b) => (a.itemCount < b.itemCount) ? 1 : -1);
+                let response = [];
+                for (let i = 0; i < data.Items.length; i++) {
+                    if (message.guild.emojis.resolve(data.Items[i].subKey) && data.Items[i].itemCount) {
+                        response.push({emoji: message.guild.emojis.resolve(data.Items[i].subKey), count: data.Items[i].itemCount});
+                    }
+                }
+                let responseMessage;
+                if (response.length) {
+                    response = response.slice(0,PAGE_SIZE);
+                    responseMessage = `${message.guild.name} react leaderboard\n`;
+                    responseMessage += `emojis with most react usage in #${message.channel.name}\n`;
+                    responseMessage += `---------------------------\n`;
+                    let emojiCount = 1;
+                    for (let i = 0; i < response.length; i++) {
+                        responseMessage += `${emojiCount}. ${response[i].emoji}\t|\tCount: ${response[i].count}\n`;
+                        emojiCount += 1;
+                    }
+                    responseMessage += `---------------------------`;
+                }
+                if (responseMessage) {
+                    message.channel.send(responseMessage);
+                }
+            }
+        });
+        message.delete();
+    }
     else if (message && message.guild && message.guild.id && message.content && message.channel && message.channel.id && message.content.toLowerCase().startsWith('!reacts')) {
         if (message.mentions && message.mentions.users && message.mentions.users.first()) {
             let params = {
                 ExpressionAttributeValues: {
-                    ':rk': message.guild.id + '#' + message.channel.id + '#' + message.mentions.users.first().id
+                    ':rk': 'emojiByUser#' + message.guild.id + '#' + message.channel.id + '#' + message.mentions.users.first().id
                 }, 
                 KeyConditionExpression: 'reactionKey = :rk',
                 TableName: config.awsDynamoDBTableName
@@ -239,7 +321,7 @@ discordClient.on('message', message => {
             let emojiId = message.content.match(/<a:.+?:\d+>|<:.+?:\d+>/)[0].match(/\d+/)[0].toString();
             let params = {
                 ExpressionAttributeValues: {
-                    ':rk': message.guild.id + '#' + message.channel.id + '#' + emojiId
+                    ':rk': 'userByEmoji#' + message.guild.id + '#' + message.channel.id + '#' + emojiId
                 }, 
                 KeyConditionExpression: 'reactionKey = :rk',
                 TableName: config.awsDynamoDBTableName
@@ -274,6 +356,16 @@ discordClient.on('message', message => {
                     }
                 }
             });
+            message.delete();
+        }
+        else {
+            let responseMessage = `Welcome to the ${message.guild.name} react leaderboard!\n`;
+            responseMessage += `You can use the following commands in the chat channel:\n`;
+            responseMessage += `\`!reacts users\` will show the users in the channel with the most overall react activity\n`;
+            responseMessage += `\`!reacts emojis\` will show the emojis in the channel with the most usage\n`;
+            responseMessage += `\`!reacts @userName\` will show the most commonly used emojis for a specific user\n`;
+            responseMessage += `\`!reacts :emojiname:\` will show the users that react with a specific emoji most often\n`;
+            message.author.send(responseMessage);
             message.delete();
         }
     }
